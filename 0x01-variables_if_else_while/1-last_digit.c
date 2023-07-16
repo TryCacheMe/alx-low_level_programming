6e2a31136bec504934b2dfcfe319ff479208fc4f
TryCacheMe <kirunyuernest35@gmail.com>
2023-07-16 05:08:08 -0700

    the last number in an array

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -6,7 +6,7 @@
   *
   * Return: Always 0 (Success)
   */
-int main(void)
+int main (void)
 {
 	int n;
 	int ld;
@@ -22,7 +22,7 @@ int main(void)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, ld);
 	}
-	else
+	else if (n < 6 != 0)
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, ld);
 	}

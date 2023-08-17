4ed0d77fc5bde35ccbd3422dac5f9138e798f667
TryCacheMe <kirunyuernest35@gmail.com>
2023-08-17 07:53:50 -0700

    2-print_strings.c

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,38 @@
+#include "variadic_functions.h"
+/**
+  * print_strings - prints strings
+  * @separator: separates between string values
+  * @n: number of arguments
+  */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int index;
+	char *strng;
+
+	va_list list;
+
+	va_start(list, n);
+
+	for (index =  0; index < n; index++)
+	{
+		strng = va_arg(list, char *);
+		if (!strng)
+		{
+			strng = "nil";
+		}
+		if (!separator)
+		{
+			printf("%s", strng);
+		}
+		else if (separator && index == 0)
+		{
+			printf("%s", strng);
+		}
+		else
+		{
+			printf("%s%s", separator, strng);
+		}
+	}
+	printf("\n");
+	va_end(list);
+}

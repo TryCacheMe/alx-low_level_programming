4186b96a66436bd5e5fb7d550acf3be63f689e8c
TryCacheMe <kirunyuernest35@gmail.com>
2023-08-29 05:05:38 -0700

    hot fix

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -3,21 +3,21 @@
 
 /**
  * print_list - Print elements of lists_t list
- * @l: the list_t l.
+ * @h: the list_t l.
  * Return: number of nodes in l
  */
-size_t print_list(const list_t *l)
+size_t print_list(const list_t *h)
 {
 	size_t nodes = 0;
 
-	while (l)
+	while (h)
 	{
-		if (l->str == NULL)
+		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
-			printf("[%d] %s\n", l->len, l->str);
+			printf("[%d] %s\n", h->len, h->str);
 		nodes++;
-		l = l->next;
+		h = h->next;
 	}
 	return (nodes);
 }
